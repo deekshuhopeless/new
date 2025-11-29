572503a7c7a5758d20bbd55cd44e651caa492ea2
chintelstechteam-maker <chintelstechteam@gmail.com>
2025-11-29 12:14:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: './src' });
+});
+app.get('/login', (req, res) => {
+    res.sendFile('login.html', { root: './src' });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
